8394686b84c835c116782701d3c969ac56898100
daalagelerua <aurele284@gmail.com>
2025-06-04 17:22:38 +0200

    feat: adding index.js

---
+++ vizza-platform/backend/models/index.js
@@ -0,0 +1,9 @@
+const Country = require('./Country');
+const VisaRequirement = require('./VisaRequirement');
+const User = require('./User');
+
+module.exports = {
+  Country,
+  VisaRequirement,
+  User
+};
